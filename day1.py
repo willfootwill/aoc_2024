504cb921d11f900bf02a732338cde1684e5d7cda
Will Foot <willfoot@gmail.com>
2024-12-04 09:47:36 -0500

    part 2

--- day1.py
+++ day1.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('~/github/aoc_2024/input/day1', header=None, sep=r'\s+')
+data = pd.read_csv('~/github/aoc_2024/input/day1', header=None, sep=r'\s+')
+df = pd.DataFrame(data)
 df[:] = np.sort(df.values, axis=0)
 df["delta"] = abs(df[0] - df[1])
 print(sum(df["delta"]))
+
+
+same_map = dict(df[1].value_counts())
+df["similar"] = df[0].apply(lambda x: x * same_map.get(x,0))
+print(sum(df["similar"]))
